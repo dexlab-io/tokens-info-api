c0284a975724462f61df997bb84f4db3f20f50cf
Alessio <alessio.d@gmail.com>
2018-03-16 20:35:25 +0100

    Adding search/symbol endpoint

--- src/ERC20Tokens/erc20-tokens.controller.ts
+++ src/ERC20Tokens/erc20-tokens.controller.ts
@@ -1,6 +1,9 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import * as path from 'path';
 import * as fs from 'fs';
+import {find, startsWith, without} from 'lodash';
+// tslint:disable-next-line:no-var-requires
+const db = require('../../assets/db.json');
 // tslint:disable-next-line:no-var-requires
 const yaml = require('js-yaml');
 const tokenYalmDirectory = path.join(__dirname, '..', '..', '/assets/ERC20-tokens/');
@@ -19,6 +22,38 @@ export class ERC20TokensController {
     } catch (e) {
       return [];
     }
+  }
+
+  @Get('/json')
+  getJson() {
+    try {
+      const res = [];
+      const json = [];
+
+      fs.readdirSync(tokenYalmDirectory).forEach(file => {
+        res.push(file.replace('.yaml', ''));
+      });
+
+      res.forEach( o => {
+        const doc = yaml.safeLoad(fs.readFileSync(`${tokenYalmDirectory}${o}.yaml`, 'utf8'));
+        json.push(doc);
+      });
+
+      fs.writeFile(tokenYalmDirectory + '../db.json', JSON.stringify(json), (err) => {
+          if (err) {
+              return err;
+          }
+      });
+
+      return json;
+    } catch (e) {
+      return [];
+    }
+  }
+
+  @Get('/search/symbol/:v')
+  search(@Param() params) {
+    return without(db.map(i => startsWith(i.symbol, params.v) ? i : undefined), undefined) || [];
   }
 
   @Get(':contractAddress')
